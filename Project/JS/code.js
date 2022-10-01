6f5fe75de1d7df09ec4135b0c96e82618134bd5e
raghavbassi <114511302+raghavbassi@users.noreply.github.com>
2022-10-01 13:10:57 -0700

    Add files via upload

--- Project/JS/code.js
+++ Project/JS/code.js
@@ -1,1 +1,6 @@
-
+function myMove() {
+    document.getElementById("rag").style.gridRow = "3"
+    document.getElementById("young").style.gridRow = "1";
+    document.getElementById("rac").style.gridRow = "4";
+    document.getElementById("gur").style.gridRow = "2";         
+}
